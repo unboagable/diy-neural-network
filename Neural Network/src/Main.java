ba8f42c56c7a7a955a15c33b44a49287c7f38e89
unboagable <changhyunmungai@gmail.com>
2016-03-25 10:26:46 -0400

    Initial

---
+++ Neural Network/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Chang-Hyun
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Neural Network/src/Neuron/Perceptron.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package Neuron;
+
+/**
+ * @author Chang-Hyun
+ *
+ */
+public class Perceptron {
+	double[] inputs  = {12 , 4};
+	double[] weights = {0.5,-1};
+
+}
