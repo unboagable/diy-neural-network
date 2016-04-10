865370b76323f89f921bcb2b411b782d07fe8a2b
unboagable <changhyunmungai@gmail.com>
2016-04-10 13:13:15 -0400

    added timer to simulation

--- Neural Network/src/singlePerceptron/PerceptronSimulation.java
+++ Neural Network/src/singlePerceptron/PerceptronSimulation.java
@@ -1,21 +1,24 @@
 package singlePerceptron;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 public class PerceptronSimulation {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				createAndShowSimulation();
+				createAndShowSimulation(true);
 			}
 		});
 	}
 	
-	private static void createAndShowSimulation() {
+	private static void createAndShowSimulation(boolean timed) {
 		// Create and set-up the window.
 		final JFrame frame = new JFrame("Perceptron Simulation");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,7 +36,15 @@ public class PerceptronSimulation {
 				frame.repaint();
 			}
 	    });
-
+		
+		Timer timer = new Timer(1000, new ActionListener() {
+		    public void actionPerformed(ActionEvent evt) {
+				simulationPanel.oneMoreTraining();
+				frame.repaint();
+		    }    
+		});
+		
+		timer.start();
 		// Display the window.
 		frame.pack();
 		frame.setVisible(true);
