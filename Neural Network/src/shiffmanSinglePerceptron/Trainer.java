17a13b36749a6efa51e27c049e911f3aba90997e
Chang-Hyun Mungai <changhyunmungai@gmail.com>
2016-06-18 15:32:28 -0400

    -

--- Neural Network/src/shiffmanSinglePerceptron/Trainer.java
+++ Neural Network/src/shiffmanSinglePerceptron/Trainer.java
@@ -7,6 +7,9 @@ class Trainer {
 	 
 	public Trainer(double x, double y, int a) {
 		scale = Math.max(x, y);
+		
+		//so values will be between 0.0 and 1.0
+		
 		inputs = new double[2];
 		inputs[0] = x/scale;
 		inputs[1] = y/scale;
